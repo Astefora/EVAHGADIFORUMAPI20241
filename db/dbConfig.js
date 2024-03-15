d8e88ffdf8ae745fd081310da2d2faae0a35737e
aste-0973 <152900534+Astefora@users.noreply.github.com>
2024-03-15 12:47:36 -0400

    lastbackend for evangadi

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,11 +1,16 @@
 const mysql2 = require("mysql2");
 const dbConnection = mysql2.createPool({
-  user: process.env.USER,
-  database: process.env.DATABASE,
-  password: process.env.PASSWORD,
-  host: " sql5.freesqldatabase.com",
-  connectionLimit: 10,
-  port: 3306,
+  // user: process.env.USER,
+  // database: process.env.DATABASE,
+  // password: process.env.PASSWORD,
+  // host: " sql5.freesqldatabase.com",
+  // connectionLimit: 10,
+  // port: "3306",
+  host: "sql5.freesqldatabase.com",
+database: "sql5691589",
+user:"sql5691589",
+ password:"ilqA8SC1zr" ,
+port: "3306"
 });
 
 // console.log(process.env.DATABASE)
